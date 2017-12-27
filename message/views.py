aee17d2b6c82c81e9c56d72306508f638d4dd8a2
samzhuwj <samzhuwj@gmail.com>
2017-12-27 22:39:05 +0800

    view, url index

--- message/views.py
+++ message/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from pusher import Pusher
+
+# instantiate pusher
+pusher = Pusher(app_id='XXX_APP_ID', key='XXX_APP_KEY', secret='XXX_APP_SECRET', cluster='XXX_APP_CLUSTER')
+
+
+# add the login required decorator, so the method cannot be accessed w/o login
+@login_required(login_url='login/')
+def index(request):
+    return render(request,'chat.html')    
--- pusher_message/urls.py
+++ pusher_message/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
+from message.views import *
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', index),
+    url(r'^admin/', admin.site.urls),
+    url(r'^login/$', views.login, {'template_name': 'login.html'}),
 ]
